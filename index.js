00f0361e5ab101cc909b4c0880763c78200d4e38
Sakthii_vijay <vj5128808@gmail.com>
2025-09-09 15:31:20 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", async (req,res)=>{
+try{
+const result= await axios.get("https://v2.jokeapi.dev/joke/Any");
+res.render("index.ejs",{
+    output1: result.data.setup,
+    output2: result.data.delivery,
+
+});
+}catch(error){
+    res.sendStatus(404).send(error.message);
+}
+});
+app.listen(port,()=>{
+    console.log(`Server listening on port ${port}`);
+})
